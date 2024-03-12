35e4937bd7a12a9c4897609657b0d133670c20c1
Marius Stern <marius-stern@gmx.de>
2024-03-12 11:39:47 +0100

    added harpoon plugin and a basic setup with keybindings for it

--- lua/nvim-marius/packer.lua
+++ lua/nvim-marius/packer.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function(use)
 			{'hrsh7th/nvim-cmp'},
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
-		}
+		},
+    use("ThePrimeagen/harpoon")
 	}
 end)
