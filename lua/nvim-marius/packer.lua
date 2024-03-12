ed25fe3e51c5e1183e9122f5397faa5ec0f890fc
Marius Stern <marius-stern@gmx.de>
2024-03-12 21:17:22 +0100

    installed nvim/dap with basic setup

--- lua/nvim-marius/packer.lua
+++ lua/nvim-marius/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
 		},
+    use('mfussenegger/nvim-dap'),
     use("ThePrimeagen/harpoon")
 	}
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,6 +114,11 @@ _G.packer_plugins = {
     path = "/home/Marius/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/home/Marius/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/Marius/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
