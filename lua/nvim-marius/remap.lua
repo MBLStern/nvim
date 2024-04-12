05a535f60988958cda03ad8535551cbb55990e2b
Marius Stern <marius-stern@gmx.de>
2024-04-12 18:42:27 +0200

    added a keyboard shortcut to quickly type a err != nil check for go

--- lua/nvim-marius/remap.lua
+++ lua/nvim-marius/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- add a quick error check for a go program
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
