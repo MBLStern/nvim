3e7a1715287b61263c1e2af1c6b2ea34f08e4a3f
Marius Stern <marius-stern@gmx.de>
2024-07-10 15:51:31 +0200

    Added vimtex plugin with minimal configuration

---
+++ after/plugin/vimtex.lua
@@ -0,0 +1,2 @@
+vim.cmd("filetype plugin on")
+vim.g.vimtex_view_method = "zathura"
--- lua/nvim-marius/packer.lua
+++ lua/nvim-marius/packer.lua
@@ -1,41 +1,49 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({ "catppuccin/nvim", as = "catppuccin" })
-	use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use({ "catppuccin/nvim", as = "catppuccin" })
+    use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate' })
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
+            { 'neovim/nvim-lspconfig' },
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'hrsh7th/cmp-cmdline'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
-            {'saadparwaiz1/cmp_luasnip'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-cmdline' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+            { 'saadparwaiz1/cmp_luasnip' },
         },
-    use{'mfussenegger/nvim-dap',
-        requires = {"rcarriga/nvim-dap-ui",
-                    "leoluz/nvim-dap-go",
-                    "nvim-neotest/nvim-nio"},
+        use { 'mfussenegger/nvim-dap',
+            requires = { "rcarriga/nvim-dap-ui",
+                "leoluz/nvim-dap-go",
+                "nvim-neotest/nvim-nio" },
         },
-    use("ThePrimeagen/harpoon")
-	}
+        use("ThePrimeagen/harpoon")
+    }
     use('Civitasv/cmake-tools.nvim')
+    use {
+        'lervag/vimtex',
+        lazy = false,
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end
+    }
 end)
