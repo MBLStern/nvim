93e6a46ac8b354bbf0cb33544cf9eed9986ee354
Marius Stern <marius-stern@gmx.de>
2024-09-23 20:36:11 +0200

    Added debug adapter config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,12 @@
 local javaConfig = {
     cmd = { '/usr/bin/jdtls' },
     root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+    init_options = {
+        bundles = {
+            vim.fn.glob(
+                "/home/Marius/ManualPackages/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+                true)
+        },
+    }
 }
 require('jdtls').start_or_attach(javaConfig)
