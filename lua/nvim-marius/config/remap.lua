2e1084131e2dc571cd5e4e863e080a4c0081f642
Marius Stern <marius-stern@gmx.de>
2025-01-28 15:54:41 +0100

    Added keymaps for quickfix next and previous

--- lua/nvim-marius/config/remap.lua
+++ lua/nvim-marius/config/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>gp", ":e #<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 
 -- add a quick error check for a go program
